c64850e5b1ed7722f055dd472026e3761ed2c236
kesapragada <136173335+kesapragada@users.noreply.github.com>
2025-08-09 18:51:16 +0530

    Delete backend directory

--- backend/queues/claimQueue.js
+++
@@ -1,9 +0,0 @@
-//CLAIMCHECK/backend/queues/claimQueue.js
-const { Queue } = require('bullmq');
-const IORedis = require('ioredis');
-
-const connection = new IORedis(process.env.REDIS_URL);
-
-const claimQueue = new Queue('claims', { connection });
-
-module.exports = claimQueue;
